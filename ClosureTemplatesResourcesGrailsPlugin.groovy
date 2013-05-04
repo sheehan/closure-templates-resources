6bef52e4298133ad8247f43bd73f68e969208fdf
Burt Beckwith <burt@burtbeckwith.com>
2013-05-04 15:38:25 -0400

    cleanup

--- ClosureTemplatesResourcesGrailsPlugin.groovy
+++ ClosureTemplatesResourcesGrailsPlugin.groovy
@@ -1,15 +1,7 @@
 class ClosureTemplatesResourcesGrailsPlugin {
-    // the plugin version
     def version = "0.1"
-    // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.5 > *"
-    // the other plugins this plugin depends on
-    def dependsOn = [resources:'1.0 > *']
     def loadAfter = ['resources']
-    // resources that are excluded from plugin packaging
-    def pluginExcludes = [
-        "grails-app/views/error.gsp"
-    ]
 
     def license = "APACHE"
     def issueManagement = [ system: "github", url: "https://github.com/sheehan/closure-templates-resources/issues" ]
@@ -18,8 +10,6 @@ class ClosureTemplatesResourcesGrailsPlugin {
     def title = "Closure Templates Resources Plugin"
     def author = "Matt Sheehan"
     def authorEmail = "mr.sheehan@gmail.com"
-    def description = '''\
-This plugin supports server-side compilation of .soy template files to JavaScript files.
-'''
+    def description = 'This plugin supports server-side compilation of .soy template files to JavaScript files.'
     def documentation = "https://github.com/sheehan/closure-templates-resources/blob/master/README.md"
 }
